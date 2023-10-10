4cede98d7e07a33464b1c3e5a180065c68c4f2d6
hector-medina <hector.medina.cabanelas@gmail.com>
2023-10-10 17:45:01 +0800

    Add title and author

--- app/App.js
+++ app/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>PEC 1</Text>
+      <Text style={styles.subTitleText}>Héctor Medina</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+  subTitleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'gray',
+  }
 });
